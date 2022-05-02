78ea5a6eb77700bddcc70bf777c243e88962217a
Ernst <alexander.ernst@unibe.ch>
2022-05-02 17:22:20 +0200

    Update DataApp.py

--- DataApp.py
+++ DataApp.py
@@ -3,13 +3,15 @@ import pandas as pd
 import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
-
+import os
 #import pydeck as pdk
 #import numpy as np#Load and Cache the data
 #from skimage.io import imread, imshow 
 #from PIL import Image
 #from glob import glob
 "Data/Example"
-data = pd.read_excel("C:/Users/aernst/Documents/GitHub/Test_App/Data/Example.xlsx")
+path = "https://raw.githubusercontent.com/alernst/Test_App/Data/Example.xlsx"
+st.text(path)
+data = pd.read_excel(path)
 fig = px.violin(data)
 st.plotly_chart(fig,use_container_width=True)
