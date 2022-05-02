4d8c478d215925978644a6534fd9656d5026aaf9
Ernst <alexander.ernst@unibe.ch>
2022-05-02 14:55:00 +0200

    Initial Test App

---
+++ DataApp.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+
+import pydeck as pdk
+import numpy as np#Load and Cache the data
+from skimage.io import imread, imshow 
+from PIL import Image
+from glob import glob
+
+data = pd.read_excel("./Data/Example.xlsx")
+
+
+fig = px.violin(data)
+st.plotly_chart(fig,use_container_width=True)
