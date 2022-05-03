e92165b073178d3dfa4e6dfbb45c1867d998620f
Ernst <alexander.ernst@unibe.ch>
2022-05-03 10:26:58 +0200

    Update DataApp.py

--- DataApp.py
+++ DataApp.py
@@ -12,21 +12,23 @@ st.write(passwords)
 st.write(usernames)
 authenticator = stauth.Authenticate(names,usernames,passwords,
     'Show','me12',cookie_expiry_days=30)
-name, authentication_status, username = authenticator.login('Login','main')
 
-if authentication_status:
-    authenticator.logout('Logout', 'main')
-    st.write('Welcome *%s*' % (name))
-    st.title('Best results ever')
-    path = "Data/Example.xlsx"
-    st.text(path)
-    data = pd.read_excel(path)
-    fig = px.violin(data)
-    st.plotly_chart(fig,use_container_width=True)
-elif authentication_status == False:
-    st.error('Username/password is incorrect')
-elif authentication_status == None:
-    st.warning('Please enter your username and password')
+with authenticator:
+    name, authentication_status, username = authenticator.login('Login','main')
+
+    if authentication_status:
+        authenticator.logout('Logout', 'main')
+        st.write('Welcome *%s*' % (name))
+        st.title('Best results ever')
+        path = "Data/Example.xlsx"
+        st.text(path)
+        data = pd.read_excel(path)
+        fig = px.violin(data)
+        st.plotly_chart(fig,use_container_width=True)
+    elif authentication_status == False:
+        st.error('Username/password is incorrect')
+    elif authentication_status == None:
+        st.warning('Please enter your username and password')
     
 
 
