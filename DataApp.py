66c1da0c53b6f48af4e2a3c15e06be4adf55e559
Ernst <alexander.ernst@unibe.ch>
2022-05-06 11:41:39 +0200

    Update DataApp.py

--- DataApp.py
+++ DataApp.py
@@ -11,13 +11,20 @@ st.title("Demo App")
 path = "Data/Example.xlsx"
 data = pd.read_excel(path)
 
+options = st.multiselect(
+     'What are your favorite colors',
+     data.columns,
+     data.columns[:2])
+
+
 points = st.checkbox('Show points')
 
+
 if points:
-    fig = px.violin(data, points="all")
+    fig = px.violin(data[options], points="all")
 
 else:
-    fig = px.violin(data)
+    fig = px.violin(data[options])
     
 st.plotly_chart(fig,use_container_width=True)
 
